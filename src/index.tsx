a5216d5ea913d76d8a7b8d76ff8e755b7c32ccd4
MuhammedJaseel <muhammedjaseelm@Muhammeds-MacBook-Air-2.local>
2023-07-24 18:04:03 +0530

    Updating App

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import App from "./app_";
+import App from "./app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
